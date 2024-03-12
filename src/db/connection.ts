79a2ff28b1c1fd6b4d7a3d953f62148c491282f3
sebas9-11 <sebastian-1999@outlook.com>
2024-03-11 21:57:32 -0500

    db config

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,10 +4,13 @@ import { Pool } from "pg"
 dotenv.config()
 
 const config = {
+  connectionString: process.env.POSTGRES_URL,
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE
 }
+
+console.log(config)
 
 export const connection = new Pool(config)
